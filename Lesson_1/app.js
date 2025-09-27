bcccd548210a18e4791d4e8e4c557e8559315a12
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2025-09-27 10:21:19 +0600

    Add files via upload

---
+++ Lesson_1/app.js
@@ -0,0 +1,48 @@
+// DOM - Document Object Model
+
+// Search
+
+const counterValue = document.getElementById("counter-value");
+const actionMinus = document.getElementById("action-minus");
+const actionReset = document.getElementById("action-reset");
+const actionPlus = document.getElementById("action-Plus");
+
+function resetCounter() {
+  counterValue.innerText = 0;
+  setCounterColor();
+}
+
+// Events
+actionReset.addEventListener("click", resetCounter);
+
+function plusCounter() {
+  let value = Number(counterValue.innerText);
+  value++;
+  counterValue.innerText = value;
+  setCounterColor();
+}
+
+actionPlus.addEventListener("click", plusCounter);
+
+function minusCounter() {
+  let value = Number(counterValue.innerText);
+  value--;
+  counterValue.innerText = value;
+  setCounterColor();
+}
+
+actionMinus.addEventListener("click", minusCounter);
+
+function setCounterColor() {
+  let value = Number(counterValue.innerText);
+
+  if (value > 0) {
+    counterValue.style.color = "lime";
+  } else if (value < 0) {
+    counterValue.style.color = "red";
+  } else {
+    counterValue.style.color = "cyan";
+  }
+}
+
+// DRY - Don't Repeat Yourself
